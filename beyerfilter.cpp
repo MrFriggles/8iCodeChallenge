dfa029c055f17c03d6a8678b7cd4e7b4b9936b50
Andrew Jane <andrewjane.acj@gmail.com>
2020-07-29 16:29:50 +1200

    Fixed debeyer filter, and added foreground extraction

--- beyerfilter.cpp
+++ beyerfilter.cpp
@@ -16,10 +16,9 @@ BeyerFilter::BeyerFilter()
 
     // Initialize the clean plate image
     QImage tmp("./CleanPlate.png");
-    cleanPlate = QImage( tmp.width()/4, tmp.height()/4, QImage::Format_ARGB32);
     debeyer(&tmp, &cleanPlate);
-    cleanPlate.save(tr("hotdog.png"), "PNG");
 
+    cleanPlate.save(tr("hotdog.png"), "PNG");
 
     gridLayout->addWidget(sourceButton, 1, 0, 3, 1);
     gridLayout->addWidget(beyerButton,  0, 0);
@@ -38,6 +37,7 @@ BeyerFilter::~BeyerFilter()
     delete beyerButton;
 }
 
+//Seems like boilerplate code...
 void BeyerFilter::chooseSource()
 {
     chooseImage(tr("Choose Source Image"), &sourceImage, sourceButton);
@@ -45,10 +45,6 @@ void BeyerFilter::chooseSource()
 
 void BeyerFilter::saveOutput()
 {
-    // Init the output image
-    outputImage = QImage(sourceImage.size(), sourceImage.format());
-    fgOutput    = QImage(sourceImage.size(), QImage::Format_ARGB32); // Difference in format so we can set it to be transparent
-
     if (debeyer(&sourceImage, &outputImage)
             && extractForeground(&outputImage, &fgOutput)
             && fgOutput.save(tr("beans.png", "PNG")))
@@ -97,6 +93,7 @@ bool BeyerFilter::debeyer(QImage *source, QImage *output)
     const int w = source->width();  // Width of source image
     const int h = source->height(); // heigh of source image
 
+
     /*
      * setPixel seems to be inefficent according to documentation; could run awfully on very large resolutions...
      *
@@ -109,31 +106,22 @@ bool BeyerFilter::debeyer(QImage *source, QImage *output)
     QRgb pixColGreen2;
     QRgb newPixCol;
 
-    int k = 0;
-    int l = 0;
+    QImage tmp = QImage(source->scaled(w/2, h/2, Qt::KeepAspectRatio));
+
+    int k = 0;  // Scaled height index
+    int l = 0;  // Scaled width index
     for (int i = 0; i < h; i += 2)
     {
         for (int j = 0; j < w; j += 2)
         {
-//            pixCol = source->pixel(j, i); // Red-1
-//            output->setPixel(j, i, pixCol & 0xff0000);
+            pixColRed    = source->pixel(j, i)     & 0xffff0000 ; // Red-1
+            pixColGreen2 = source->pixel(j+1, i)   & 0xff00ff00; // Green-2
+            pixColGreen1 = source->pixel(j, i+1)   & 0xff00ff00; // Green-3
+            pixColBlue   = source->pixel(j+1, i+1) & 0xff0000ff; // Blue-4
 
-//            pixCol = source->pixel(j+1, i); // Green-2
-//            output->setPixel(j+1, i, pixCol & 0x00ff00);
+            newPixCol    = 0xff000000 | pixColRed | pixColBlue | ((pixColGreen1 + pixColGreen2) / 2);
 
-//            pixCol = source->pixel(j, i+1); // Green-3
-//            output->setPixel(j, i+1, pixCol & 0x00ff00);
-
-//            pixCol = source->pixel(j+1, i+1); // Blue-4
-//            output->setPixel(j+1, i+1, pixCol & 0x0000ff);
-
-            pixColRed = source->pixel(j, i) & 0xffff0000 ; // Red-1
-            pixColGreen2 = source->pixel(j+1, i) & 0xff00ff00; // Green-2
-            pixColGreen1 = source->pixel(j, i+1) & 0xff00ff00; // Green-3
-            pixColBlue = source->pixel(j+1, i+1) & 0xff0000ff; // Blue-4
-            newPixCol = qRgba(pixColRed,((pixColGreen1 + pixColGreen2) / 2), pixColBlue, (unsigned int)0xff000000);
-            printf("k: %d \t l: %d\n", k, l);
-            //            output->setPixel(l, k, newPixCol);
+            tmp.setPixel(l, k, newPixCol);
 
             if (j % 2 == 0)
             {
@@ -141,50 +129,60 @@ bool BeyerFilter::debeyer(QImage *source, QImage *output)
             }
         }
 
+        l = 0;
+
         if (i % 2 == 0)
         {
             k++;
         }
     }
 
-    output->save("test.png", "PNG");
+    *output = tmp;
 
     return true;
 }
 
 bool BeyerFilter::extractForeground(QImage *source, QImage *output)
 {
+    QImage tmp = QImage(source->size(), QImage::Format_ARGB32);
+
     const int w = source->width();  // Width of source image
     const int h = source->height(); // heigh of source image
 
-    for (int i = 0; i < h; i += 2)
+    for (int i = 0; i < h; i++)
     {
-        for (int j = 0; j < w; j += 2)
+        for (int j = 0; j < w; j++)
         {
-            QRgb sourceGreen = qGreen(source->pixel(j+1, i));
-            QRgb sourceRed   = qRed(source->pixel(j, i));
-            QRgb sourceBlue  = qBlue(source->pixel(j+1, i+1));
-            QRgb cleanGreen  = qGreen(cleanPlate.pixel(j+1, i));
+            QRgb sourcePixelAll  = source->pixel(j, i);
+            QRgb sourcePixelR    = qRed(sourcePixelAll);
+            QRgb sourcePixelG    = qGreen(sourcePixelAll);
+            QRgb sourcePixelB    = qBlue(sourcePixelAll); // Unused
 
-            double tol = 0.15; // Make this change with slider
-            double ratio = ((double)sourceGreen / (double)cleanGreen);
+            QRgb cleanPixel      = qGreen(cleanPlate.pixel(j, i));
 
-            if (sourceGreen > sourceRed && sourceGreen > sourceBlue
-                    && (((1 - tol) <= ratio) && (ratio <= (1 + tol))))
+            double tol    = 0.2; // Make this change with slider
+            double ratioG = ((double)sourcePixelG  / (double)cleanPixel);
+
+
+            /*
+             * Only check for green dominance over red, as
+             * checking for blue could leave blue artifacts around
+             * from the debeyering in the background of the extracted image
+             */
+            if (((1 - tol) <= ratioG) && (ratioG <= (1 + tol))
+                    && (sourcePixelG > sourcePixelR))
+
             {
-                output->setPixel(j,   i,   0x00000000);
-                output->setPixel(j+1, i,   0x00000000);
-                output->setPixel(j,   i+1, 0x00000000);
-                output->setPixel(j+1, i+1, 0x00000000);
+                tmp.setPixel(j, i, 0x00000000);
             }
             else
             {
-                output->setPixel(j,   i,   source->pixel(j,   i));
-                output->setPixel(j+1, i,   source->pixel(j+1, i));
-                output->setPixel(j,   i+1, source->pixel(j,   i+1));
-                output->setPixel(j+1, i+1, source->pixel(j+1, i+1));
+                tmp.setPixel(j, i, source->pixel(j, i));
             }
         }
     }
+
+    *output = tmp;
+
     return true;
 }
